0a0a89f2e6d20655fcac0f180b91d311f21a14ed
Srijit <srijitd248@gmail.com>
2026-03-26 21:58:02 +0530

    Time: 4 ms (25.07%), Space: 113.8 MB (27.6%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        double current_sum = 0;
+        
+        for (int i = 0; i < k; ++i) {
+            current_sum += nums[i];
+        }
+        
+        double max_sum = current_sum;
+        
+        for (int i = k; i < nums.size(); ++i) {
+            current_sum = current_sum - nums[i - k] + nums[i];
+            if (current_sum > max_sum) {
+                max_sum = current_sum;
+            }
+        }
+        
+        return max_sum / k;
+    }
+};
